a94962335126c5589ea0632c79316fcf80f0893f
Esther de Freitas <ensf1@aluno.ifal.edu.br>
2021-06-25 13:17:03 -0300

    Descrição dos personagens e suas habilidades

---
+++ main.py
@@ -0,0 +1,60 @@
+# personagens
+personagens = []
+#Guerreiro tanker
+elias = {
+    'nome': 'Elias, o Bárbaro',
+    'defesa': 15,
+    'força': 10,
+    'destreza': 5,
+    'poder Mágico': 0,
+    'habilidade1': 'Soco de uma polegada', #Dano
+    'habilidade2': '', #
+    'habilidade3': 'Carapaça reforçada', #Dobra a defesa no próximo round
+    'habilidade4': '', #
+}
+personagens.append(conan)
+#Mago
+pedro = {
+    'nome': 'Pedro Frozen, o Mago',
+    'força': 0,
+    'destreza': 0,
+    'poder Mágico': 20,
+    'defesa': 10,
+    'habilidade1': 'Tempestade de neve', #Dano
+    'habilidade2': 'Chuva de granizos', #Congela o inimigo por 1 turno
+    'habilidade3': 'Envólucro de gelo', #Reduz o dano recebido no próximo ataque inimigo
+    'habilidade4': 'Brisa congelante', #
+}
+personagens.append(elza)
+#Arqueiro
+esther = {
+    'nome': 'Esther, a Arqueira',
+    'força': 10,
+    'destreza': 15,
+    'poder Mágico': 0,
+    'defesa': 5,
+    'habilidade1': 'Flecha perfurante', #Dano
+    'habilidade2': 'Flecha taciturna', #Impede o inimigo de usar habilidades por 2 turnos
+    'habilidade3': 'Aura galvânica', #Esquiva da próxima habilidade inimiga (taxa de sucesso)
+    'habilidade4': 'Maestria letal', #Aumenta o dano do ataque básico por 2 turnos
+}
+personagens.append(legolas)
+#Samurai
+Joberval = {
+    'nome': 'Joberval, o Samurai',
+    'força': 15,
+    'destreza': 10,
+    'poder Mágico': 2,
+    'defesa': 3,
+    'habilidade1': 'Katana incisiva', # Dano
+    'habilidade2': 'Lâmina corrompida', #Diminui a defesa do inimigo no próximo round
+    'habilidade3': 'Espírito da espada', #Bloqueia o dano da próxima habilidade inimiga (porcentagem de dano)
+    'habilidade4': '', #
+
+}
+personagens.append(tomoe)
+# for personagem in personagens:
+# #     for k, v in personagem.items():
+# #         print(k +": " +str(v))
+
+
